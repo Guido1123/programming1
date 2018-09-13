d20bd0de5a06632241f49a9e71e520016694c577
Guido1123 <guido.g.jansen@student.hu.nl>
2018-09-13 09:59:50 +0200

    les 4 gemaakt

---
+++ Les4/getallen.py
@@ -0,0 +1,8 @@
+CijfICORE = 6
+CijfPROG = 6
+CijfCNS = 7
+som = CijfICORE + CijfPROG + CijfCNS
+gemiddelde = som / 3
+beloning = 30 * som
+overzicht = 'Mijn cijfers (gemiddeld een + st(gemiddelde) + 6 ') 'leveren een beloning van € '+ str(beloning) + 'op !'
+print(beloning)
---
+++ week 2/lists.py
@@ -0,0 +1,5 @@
+favorieten = ['michael jackson']
+favorieten.append ('ravenscode')
+favorieten[1] = 'Toto'
+print(favorieten)
+
