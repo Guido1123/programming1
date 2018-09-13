4791d152a44ee81cc5ad71f076a5adc4bb02448b
Guido1123 <guido.g.jansen@student.hu.nl>
2018-09-13 11:01:49 +0200

    operators

---
+++ Pe4_2/pe4.3.py
@@ -0,0 +1,4 @@
+uurloon=input('hoeveel verdien je per uur: ')
+Uren = input('hoeveel uur heb je gewerkt: ' )
+Salaris = float(uurloon) * float(Uren)
+print(Uren + ' hoeveel uur heb je gewerkt ' + str(Salaris) + ' euro op ')
