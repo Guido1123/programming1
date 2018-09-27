f7c48b7cbf499e00c636a2e52190d9e478bb86e5
Guido1123 <guido.g.jansen@student.hu.nl>
2018-09-27 10:16:21 +0200

    Formatting Les 7

--- Les 6/NS Planner.py
+++ Les 6/NS Planner.py
@@ -8,23 +8,25 @@ def standaardprijs(afstandKM):
     return getal
 
 def ritprijs(leeftijd, weekendrit, afstandKM):
-    if weekendrit == ('zaterdag') or weekendrit == ('zondag'):
-        print('true')
+    if weekendrit == 'zaterdag' or weekendrit == 'zondag':
+        weekendrit = True
         if leeftijd > 64 or leeftijd < 12:
-            print(standaardprijs(afstandKM)-(standaardprijs(afstandKM)/100*35))
+            return standaardprijs(afstandKM)-(standaardprijs(afstandKM)/100*35)
         else:
-                print(standaardprijs(afstandKM)-(standaardprijs(afstandKM)/100*40))
+            return standaardprijs(afstandKM)-(standaardprijs(afstandKM)/100*40)
     else:
-        print("False")
+        weekendrit = False
         if leeftijd < 12 or leeftijd >= 65:
-            print(standaardprijs(afstandKM)-(standaardprijs(afstandKM)/100*30))
+            return standaardprijs(afstandKM)-(standaardprijs(afstandKM)/100*30)
         else:
-            print(standaardprijs(afstandKM))
+            return standaardprijs(afstandKM)
     return
+
+
 afstandKM = eval(input('hoeveel kilometer gaat u reizen?: '))
 print(standaardprijs(afstandKM))
 weekendrit =(input('op welke dag gaat u reizen?: '))
 leeftijd = eval(input('hoe oud bent u?: '))
-ritprijs(leeftijd, weekendrit, afstandKM)
+print(ritprijs(leeftijd, weekendrit, afstandKM))
 
 
